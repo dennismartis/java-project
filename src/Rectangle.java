07ef26707627a0405cfd2d0c9f877367a4b84746
dennismartis <martisdennis@gmail.com>
2018-05-25 17:18:13 +0000

    Initial java commit

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+  public int length;
+  public int width;
+
+  public Rectangle(int lenght, int width:) {
+    this.lenght = length;
+    this.width = width
+  }
+
+  public int getArea() {
+    return length * width;
+  }
+
+  public int getPeremeter() {
+    return 2 * (length + width);
+  }
+}
