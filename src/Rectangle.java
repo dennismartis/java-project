8bb0e12854eacd84caf85e71514c6dc1c9a433ae
dennismartis <martisdennis@gmail.com>
2018-05-25 18:42:15 +0000

    replaced Rectangle.java and Rectangulator.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,16 +2,16 @@ public class Rectangle {
   public int length;
   public int width;
 
-  public Rectangle(int lenght, int width:) {
-    this.lenght = length;
-    this.width = width
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
   }
 
   public int getArea() {
     return length * width;
   }
 
-  public int getPeremeter() {
+  public int getPerimeter() {
     return 2 * (length + width);
   }
 }
